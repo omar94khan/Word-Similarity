d44e10bbdc348354e76a1e53db75d6d18cb24106
omar94khan <95233527+omar94khan@users.noreply.github.com>
2023-03-10 22:11:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def main():
     # titles = pd.DataFrame(url)
     resp = requests.get(url)
     
-    data = list(resp.text.split('\n'))
+    data = list(resp.text.split('\n'))[1:-2]
     st.write(data)
     titles =[]
     for i in range(0,len(data)):
