297b6c23b3a6477a7e96924a7abefb1ed6b0bf61
omar94khan <95233527+omar94khan@users.noreply.github.com>
2023-03-11 15:54:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -107,8 +107,8 @@ def main():
     if bow_c == True:
         vectorizer = CountVectorizer(binary=True)
         embedding = vectorizer.fit(titles).transform(titles).toarray()
-        result = find_top_n_results(titles, text, embedding, vectorizer, root_method = 'stem', n=n_words, stop_word_removal=True).sort_values('Similarity', ascending=False, ignore_index=True)
-        st.write(result)
+        bow_result = find_top_n_results(titles, text, embedding, vectorizer, root_method = 'stem', n=n_words, stop_word_removal=True)
+        st.write(bow_result.sort_values('Similarity', ascending=False, ignore_index=True))
 
     models = dict()
 
@@ -129,34 +129,5 @@ def main():
         st.write('Status of ',i,': ',j)
 
     st.write(result)
-
-    # if file is not None:
-    #     df = pd.read_csv(file)
-    #     st.write("The dataset you uploaded is:")
-    #     st.write(df)
-    #     st.write("The dataset is of shape: ", df.shape)
-
-    #     df2 = pd.DataFrame()
-        
-    #     columns = ['Time','V1','V2','V3','V4','V5','V6','V7','V8','V9','V10','V11','V12',
-    #                 'V13','V14','V15','V16','V17','V18','V19','V20','V21','V22','V23',
-    #                 'V24','V25','V26','V27','V28','Amount']
-
-    #     for col in columns:
-    #         df2[col] = df[col]
-
-    #     df2 = pd.DataFrame(MinMaxScaler().fit(df2).transform(df2), columns=df2.columns)
-        
-    #     classifier = pickle.load(open('RandomForrestClassifier_df4x.pkl', 'rb'))
-    #     result_df = pd.DataFrame(classifier.predict_proba(df2))[1]
-            
-    #     st.write("Output DataFrame depicting probability of the transaction being fraudulant.")
-    #     st.write(result_df)
-    #     st.write("Result shape: ", result_df.shape)
-
-    #     st.download_button(label="Download the results",
-    #                         data = result_df.to_csv().encode('utf-8'),
-    #                         file_name = "fraud_probability.csv",
-    #                         mime = 'text/csv')
 
 main()
