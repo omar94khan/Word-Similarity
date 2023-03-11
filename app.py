d517d31bd3a66ec7a559c330c600ad34e46688f8
omar94khan <95233527+omar94khan@users.noreply.github.com>
2023-03-11 16:05:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -132,9 +132,9 @@ def main():
 
     st.write(result)
     if bow_c == True:
-        result = result.append({['Bag of Words','Headline'] : bow_result['Result'],
+        result = result.add_suffix({['Bag of Words','Headline'] : bow_result['Result'],
                        ['Bag of Words','Similarity'] : bow_result['Similarity']
-                       }, ignore_index = True)
+                       })
 
     for i,j in model_list:
         st.write('Status of ',i,': ',j)
